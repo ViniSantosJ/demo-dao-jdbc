ec079e7c0560bf79288d2b1f38d9632ad8450863
Vinicius Santos <vinicius.santosj95@gmail.com>
2023-02-17 21:51:25 -0300

    Reusing instantiation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -6,7 +6,9 @@ import model.dao.impl.SellerDaoJDBC;
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
+
         return new SellerDaoJDBC(DB.getConnection());
+        
     }
 
 }
