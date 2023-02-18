cd0e57c3096ecc4d824d4286550ff481119823a4
Vinicius Santos <vinicius.santosj95@gmail.com>
2023-02-18 20:47:45 -0300

    Adicionado createDepartmentDao ao DaoFactory e criado DepartmentDaoJDBC

---
+++ src/application/Program2.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        sc.close();
+    }
+}
