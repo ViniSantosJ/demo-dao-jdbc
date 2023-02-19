0fbd8d4b1b46c830cdbea47cc65158b9a517a96f
Vinicius Santos <vinicius.santosj95@gmail.com>
2023-02-18 21:25:19 -0300

    Função findById department implementada

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -11,6 +11,7 @@ public class Program2 {
 
         Scanner sc = new Scanner(System.in);
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Department department = null;
 
         System.out.println("Digite o nome de um novo departamento: ");
         String n = sc.nextLine();
@@ -27,6 +28,12 @@ public class Program2 {
         System.out.println("=== TESTE 2: department delete ===");
         departmentDao.deleteById(id);
 
+        System.out.println("\nDigite um id para localizar: ");
+        id = sc.nextInt();
+        System.out.println();
+        System.out.println("=== TESTE 3: department findById ===");
+        department = departmentDao.findById(id);
+        System.out.println(department);
 
         sc.close();
     }
